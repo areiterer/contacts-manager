3fa5d34420ff40ef978f9af91850065ca0e933a4
Andreas Reiterer <reiterer.a@gmail.com>
2017-11-09 19:45:13 +0100

    display the Contact component in the App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Contact from "./components/Contact";
+
 class App extends Component {
   render() {
     return (
@@ -10,9 +12,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to our Contact Manager</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Contact />
       </div>
     );
   }
