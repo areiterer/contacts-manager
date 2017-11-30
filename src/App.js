20f822bf058475e3e189f16b83b905d2eeff5f1c
Andreas Reiterer <reiterer.a@gmail.com>
2017-11-30 18:21:39 +0100

    fetch contact data from REST API into state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import axios from "axios";
+
 import ContactList from "./components/ContactList";
 
-const contacts = [
-  { id: 1, name: "Leanne Graham" },
-  { id: 2, name: "Ervin Howell" },
-  { id: 3, name: "Clementine Bauch" },
-  { id: 4, name: "Patricia Lebsack" }
-];
-
 class App extends Component {
+  // default state object
+  state = {
+    contacts: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        // create an array of contacts only with relevant data
+        const newContacts = response.data.map(c => {
+          return {
+            id: c.id,
+            name: c.name
+          };
+        });
+
+        // create a new "state" object without mutating
+        // the original state object.
+        const newState = Object.assign({}, this.state, {
+          contacts: newContacts
+        });
+
+        // store the new state object in the component's state
+        this.setState(newState);
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,7 +44,7 @@ class App extends Component {
           <h1 className="App-title">React Contact Manager</h1>
         </header>
 
-        <ContactList contacts={contacts} />
+        <ContactList contacts={this.state.contacts} />
       </div>
     );
   }
